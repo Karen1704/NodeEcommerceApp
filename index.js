d4289d48802daf81980dcb11dd1d251be6eecc66
Karen1704 <kar.ghalachyan@gmail.com>
2023-07-28 17:30:31 +0400

    Stripe implementation

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const dotenv = require('dotenv');
 const userRouter = require('./routes/user');
 const productRouter = require('./routes/product');
 const cartRouter = require('./routes/cart');
-const orderRouter = require('./routes/order')
+const orderRouter = require('./routes/order');
+const stripeRouter = require('./routes/stripe');
+const cors = require("cors");
 
 dotenv.config();
 
@@ -15,10 +17,14 @@ mongoose.connect(process.env.MONGO_URL)
 const app = express();
 app.use(express.json());
 
+app.use(cors());
+
 app.use('/api/users',userRouter);
 app.use('/api/products',productRouter);
 app.use('/api/cart',cartRouter);
 app.use('/api/orders',orderRouter);
+app.use('/api/orders',orderRouter);
+app.use("/api/checkout", stripeRouter);
 
 app.listen(process.env.PORT || 3000,()=>{
     console.log("Server is running");
